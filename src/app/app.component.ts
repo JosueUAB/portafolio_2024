31730e5c0197e9ccf96df7f40f2ddef34e3cbc4a
josueuab <josue.arista@uab,edu,bo>
2024-07-29 13:15:44 -0400

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,32 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { HeaderComponent } from './pages/header/header.component';
+import { FooterComponent } from "./pages/footer/footer.component";
+import { HomeComponent } from './pages/home/home.component';
+import { SobreMiComponent } from './pages/sobre-mi/sobre-mi.component';
+import { ProyectosComponent } from './pages/proyectos/proyectos.component';
+import { ContactosComponent } from './pages/contactos/contactos.component';
+import { HabilidadesComponent } from './pages/habilidades/habilidades.component';
+import { initFlowbite } from 'flowbite';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    SobreMiComponent,
+    ProyectosComponent,
+    ContactosComponent,
+    HabilidadesComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'portafolio';
+  ngOnInit(): void {
+    initFlowbite();
+  }
 }
